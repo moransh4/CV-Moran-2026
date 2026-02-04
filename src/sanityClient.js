3dfab94f1f9e56d635c02e8801289d65114da30b
moransh4 <moransh4@gmail.com>
2026-02-04 08:35:38 +0200

    Updates

--- src/sanityClient.js
+++
@@ -1,9 +0,0 @@
-import sanityClient from '@sanity/client'
-
-export default sanityClient({
-  projectId: '69278jhg', // תחליפי ל-ID שלך
-  dataset: 'production',
-  useCdn: false, // השתמש ב-`false` כדי לקבל נתונים עדכניים תמיד
-  apiVersion: '2023-01-01',
-  apiHost: 'https://api.sanity.io', // Use the dev server as proxy
-})
